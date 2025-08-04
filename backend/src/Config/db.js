6be3407a27494b472f059b33876cd5a32ef98ac1
weiliams596 <mhy596520@gmail.com>
2025-08-04 15:01:12 +0500

    Reset config

--- backend/src/Config/db.js
+++ backend/src/Config/db.js
@@ -1,17 +1,17 @@
-const {Sequelize} = require('sequelize');
+const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize('postgresql://full_stuck_final_db_user:ytI3D0a5EBnNRkUidtCbYBYBB9kkOcZY@dpg-d287ipripnbc739ei5hg-a.oregon-postgres.render.com/full_stuck_final_db', {
+const sequelize = new Sequelize(process.env.DB_CONNECTED, {
   dialect: 'postgres',
   protocol: 'postgres',
   logging: false, // 可选：关闭 SQL 日志
-  dialectOptions:{
-    ssl:{
-      require:true,
-      rejectUnauthorized:false
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
     }
   },
-  pool:{
+  pool: {
     max: 5,
     min: 0,
     acquire: 30000,
